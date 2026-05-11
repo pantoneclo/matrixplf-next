46c7cad4f5c5d043080e8c7687b786569304402d
Atiqur Rahman (Rony) <jewel@matrixapparels.com>
2026-05-11 11:14:12 +0600

    update

--- components/footer.tsx
+++ components/footer.tsx
@@ -48,7 +48,7 @@ export function Footer() {
           <div className="col-span-2 lg:col-span-2">
             <Link href="/" className="group">
               <img
-                src="/Matrix-Platform-Logo-2.png"
+                src="/MATRIX-PLATFORM-LTD.png"
                 alt="Logo"
                 loading="lazy"
                 className="w-[300px] sm:w-[300px] lg:w-[300px] h-auto object-contain"
